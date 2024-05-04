f811104be3d0e125795ff30976e729b590777a26
RahimliZakir <zakirer@code.edu.az>
2024-05-04 04:09:34 +0400

    Done!!!

---
+++ Project.Solution/Project.ConsoleApp/Classes/UploadService.cs
@@ -0,0 +1,28 @@
+﻿namespace Project.ConsoleApp.Classes;
+// 1st Way: Create 'private' field, then return this with method.
+class UploadService
+{
+    // We set the constructor 'private',for not creating instances of this class.
+    private UploadService() { }
+
+    private static UploadService _instance;
+
+    // Thread-safe Locking. (Not-Parallel)
+    private static readonly object _lock = new object();
+
+    public static UploadService Instance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new UploadService();
+                }
+            }
+        }
+
+        return _instance;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Singleton-Design-Pattern-CSharp
+
+1. [Docs Source](https://www.tutorialsteacher.com/csharp/singleton)
+2. [Video Source](https://www.youtube.com/watch?v=r6Y0SmbufmU)
